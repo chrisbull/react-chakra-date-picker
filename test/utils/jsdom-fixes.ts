5064e147e8ce1b9c8f46fd0df07fd4bef4bebc6a
Chris Bull <chris@chrisbull.com>
2021-03-05 05:31:49 -0800

    fix tests

--- test/utils/jsdom-fixes.ts
+++ test/utils/jsdom-fixes.ts
@@ -1,3 +1,4 @@
+// https://jestjs.io/docs/en/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 Object.defineProperty(window, 'matchMedia', {
   writable: true,
   value: jest.fn().mockImplementation(query => ({

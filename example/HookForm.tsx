25e4f4b14ab71be233a6994296f21abab582f808
Chris Bull <chris@chrisbull.com>
2021-03-31 06:24:24 -0700

    add form devtool

--- example/HookForm.tsx
+++ example/HookForm.tsx
@@ -7,13 +7,14 @@ import {
   FormLabel,
   Stack,
 } from '@chakra-ui/react'
+import { DevTool } from '@hookform/devtools'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as React from 'react'
 import 'react-app-polyfill/ie11'
-import { useForm } from 'react-hook-form'
+import { FormProvider as HookFormProvider, useForm } from 'react-hook-form'
 import validator from 'validator'
 import * as z from 'zod'
-import { DateRangeInput, DateSingleInput } from '../.'
+import { DateRangeInput, DateSingleInput } from '../src'
 
 const dateValidation = z
   .string()
@@ -31,10 +32,6 @@ const schema = z.object({
 type Schema = z.infer<typeof schema>
 
 export function HookForm() {
-  const { handleSubmit, errors, register, formState } = useForm({
-    resolver: zodResolver(schema),
-  })
-
   function onSubmit(values: Schema) {
     return new Promise(resolve => {
       setTimeout(() => {
@@ -44,27 +41,36 @@ export function HookForm() {
     })
   }
 
-  return (
-    <Box as="form" onSubmit={handleSubmit(onSubmit)}>
-      <Stack spacing={5} my={5}>
-        <FormControl isInvalid={!!errors?.someDate}>
-          <FormLabel htmlFor="someDate">Some Date</FormLabel>
-          <DateSingleInput ref={register} id="someDate" name="someDate" />
-          <FormErrorMessage>{errors?.someDate?.message}</FormErrorMessage>
-        </FormControl>
+  const methods = useForm({
+    resolver: zodResolver(schema),
+  })
 
-        <FormControl isInvalid={!!errors?.startDate || !!errors?.endDate}>
-          <FormLabel>Date Range</FormLabel>
-          <DateRangeInput startRef={register} endRef={register} />
-          <FormHelperText>Helper text goes here</FormHelperText>
-          <FormErrorMessage>
-            {errors?.startDate?.message || errors?.endDate?.message}
-          </FormErrorMessage>
-        </FormControl>
-      </Stack>
-      <Button type="submit" colorScheme="teal" isLoading={formState.isSubmitting}>
-        Submit
-      </Button>
-    </Box>
+  const { handleSubmit, errors, register, control, formState } = methods
+
+  return (
+    <HookFormProvider {...methods}>
+      <DevTool control={control} />
+      <Box as="form" onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={5} my={5}>
+          <FormControl isInvalid={!!errors?.someDate}>
+            <FormLabel htmlFor="someDate">Some Date</FormLabel>
+            <DateSingleInput ref={register} id="someDate" name="someDate" />
+            <FormErrorMessage>{errors?.someDate?.message}</FormErrorMessage>
+          </FormControl>
+
+          <FormControl isInvalid={!!errors?.startDate || !!errors?.endDate}>
+            <FormLabel>Date Range</FormLabel>
+            <DateRangeInput startRef={register} endRef={register} />
+            <FormHelperText>Helper text goes here</FormHelperText>
+            <FormErrorMessage>
+              {errors?.startDate?.message || errors?.endDate?.message}
+            </FormErrorMessage>
+          </FormControl>
+        </Stack>
+        <Button type="submit" colorScheme="teal" isLoading={formState.isSubmitting}>
+          Submit
+        </Button>
+      </Box>
+    </HookFormProvider>
   )
 }
